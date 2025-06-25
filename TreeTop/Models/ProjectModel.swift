ba1c9f1bc90686e73e6c232722ee695eb9766296
agsanchez02 <sancheza24@southernct.edu>
2025-06-25 11:30:12 -0400

    adjusted the projectmodel strucutre to be able to work with the projectmanager file

--- TreeTop/Models/ProjectModel.swift
+++ TreeTop/Models/ProjectModel.swift
@@ -6,20 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 
+@Model
 //creating the blueprint to what a single project is in the app
-struct Project: Codable, Identifiable {
-    let id: UUID //this gives each project a unique identifier
+class Project {
+    var id: UUID
     var name: String //will store the name the user enters when creating a project
     var date: Date //stores the full date and time the user selects when creating the project
+    var folderName: String
     var imagePaths: [String] //stores the file paths as string to any images associated with the project. when the user adds a photo, we'll save it to disk and append the file path
     var canopyCoverageResults: [Double] //store the percentage canopy coverage values
     
     //defining an initialization to create a new project
-    init(name: String, date: Date) {
+    init(name: String, date: Date, folderName: String) {
         self.id = UUID()
         self.name = name
         self.date = date
+        self.folderName = folderName
         self.imagePaths = []
         self.canopyCoverageResults = []
     }
