ffe014826c299d85d9102e7760c7b299214ceeeb
agsanchez02 <sancheza24@southernct.edu>
2025-06-25 11:29:19 -0400

    Created the function to create a newproject as an object, save a captured image to the created project, and load the image once you're inside the folder to view it

--- TreeTop/Managers/ProjectManager.swift
+++ TreeTop/Managers/ProjectManager.swift
@@ -6,4 +6,67 @@
 //
 
 import Foundation
+import SwiftData
+import UIKit
 
+
+class ProjectManager {
+    static var shared: ProjectManager! //allows the instance to access the ProjectManage from anywhere
+    
+    let modelContext: ModelContext
+    
+    //initializes the manage with the given SwiftData model context
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+    }
+    
+    //this creates a unique folder name using the project name the user inputs and creates a new UUID
+    func createProject(name: String, date: Date) -> Project {
+        let folderName = "\(name) - \(UUID().uuidString)"
+        let newProject = Project(name: name, date: date, folderName: folderName) //intializes the instance
+        
+        let folderURL = FileManager.default.urls(for:.documentDirectory, in: .userDomainMask)[0].appendingPathComponent(folderName) //creates the URL for the project's folder
+        
+        try? FileManager.default.createDirectory(at: folderURL, withIntermediateDirectories: true) //creates a folder on disk at the built URL
+        
+        modelContext.insert(newProject) //inserts the new project into the SwiftData model
+        return newProject
+    }
+    
+    //creating the function to save the captured photo to the newly created project folder
+    func saveImage(_ image: UIImage, to project:Project) -> Bool{
+        let folderURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) [0] .appendingPathComponent(project.folderName)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd_HH-mm-ss"
+        
+        let fileName = "image_\(dateFormatter.string(from: Date())).jpg"
+        let fileURL = folderURL.appendingPathComponent(fileName)
+        
+        guard let imageData = image.jpegData(compressionQuality: 0.9) else {
+            return false
+        }
+        
+        do {
+            try imageData.write(to: fileURL)
+            return true
+        } catch {
+            print("Error saving image: \(error)")
+            return false
+        }
+    }
+    
+    func loadImages(for project: Project) -> [UIImage] {
+        var images: [UIImage] = []
+        
+        let folderURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask) [0] .appendingPathComponent(project.folderName)
+        if let fileURLs = try? FileManager.default.contentsOfDirectory(at: folderURL, includingPropertiesForKeys: nil) {
+            for fileURL in fileURLs {
+                if let image = UIImage(contentsOfFile: fileURL.path) {
+                    images.append(image)
+                }
+            }
+        }
+        return images
+    }
+}
