9e10ff08a85498d3e0aa54798be6d346ac0ea4e3
agsanchez02 <sancheza24@southernct.edu>
2025-06-25 20:15:35 -0400

    initializing the shared instance for projectmanager swift file

--- TreeTop/TreeTopApp.swift
+++ TreeTop/TreeTopApp.swift
@@ -19,7 +19,9 @@ struct TreeTopApp: App {
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            ProjectManager.shared = ProjectManager(modelContext: container.mainContext)
+            return container
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
