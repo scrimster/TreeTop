dfb215129c937b0991163839e1cb70d8c5ab4838
agsanchez02 <sancheza24@southernct.edu>
2025-06-29 01:59:04 -0400

    Creating existingprojectview & foldercontents view to see if project folders and their images show up (they do), try to fix code but ended up not needing it, will delete commented out blocks later

--- TreeTop/Models/ProjectModel.swift
+++ TreeTop/Models/ProjectModel.swift
@@ -15,8 +15,12 @@ class Project {
     var name: String //will store the name the user enters when creating a project
     var date: Date //stores the full date and time the user selects when creating the project
     var folderName: String
-    var imagePaths: [String] //stores the file paths as string to any images associated with the project. when the user adds a photo, we'll save it to disk and append the file path
-    var canopyCoverageResults: [Double] //store the percentage canopy coverage values
+    
+    //@Attribute(.transformable(by: StringArrayTransformer.self))
+    var imagePaths: [String] = [] //stores the file paths as string to any images associated with the project. when the user adds a photo, we'll save it to disk and append the file path
+    
+    //@Attribute(.transformable(by: DoubleArrayTransformer.self))
+    //var canopyCoverageResults: [Double] = [] //store the percentage canopy coverage values
     
     //defining an initialization to create a new project
     init(name: String, date: Date, folderName: String) {
@@ -24,7 +28,10 @@ class Project {
         self.name = name
         self.date = date
         self.folderName = folderName
-        self.imagePaths = []
-        self.canopyCoverageResults = []
+        //self.imagePaths = []
+    }
+    
+    var folderURL: URL? {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent(folderName)
     }
 }
