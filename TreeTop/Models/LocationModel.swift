32d0dbd4a741bc90b95b3e5385b8439a7a3486a2
agsanchez02 <sancheza24@southernct.edu>
2025-07-20 02:03:19 -0400

    to get coordinates without the need of internet

---
+++ TreeTop/Models/LocationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationModel.swift
+//  TreeTop
+//
+//  Created by Ashley Sanchez on 7/17/25.
+//
+
+import Foundation
+
+struct LocationModel: Codable {
+    let latitude: Double
+    let longitude: Double
+}
