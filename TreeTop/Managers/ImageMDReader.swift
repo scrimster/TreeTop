0455c53301cf2260d8f53cd6c840458de7925b0d
agsanchez02 <sancheza24@southernct.edu>
2025-08-03 16:42:35 -0400

    extracting the coordinates of the first and last photo of each diagonal image collection

--- TreeTop/Managers/ImageMDReader.swift
+++ TreeTop/Managers/ImageMDReader.swift
@@ -9,28 +9,33 @@ import Foundation
 import CoreLocation
 import ImageIO
 
-struct PhotoCoordinates {
+struct ImageMDReader {
     //set-up function to access GPS metadata
-    static func extract(from url: URL) ->CLLocationCoordinate2D? {
+    static func extract(from url: URL) -> Coordinate? {
         guard
             let source = CGImageSourceCreateWithURL(url as CFURL, nil),
             let metadata = CGImageSourceCopyPropertiesAtIndex(source, 0, nil) as? [CFString: Any],
             let gps = metadata[kCGImagePropertyGPSDictionary] as? [CFString: Any]
+
         else {
+            print("Failed to extract GPS metadata from: \(url.lastPathComponent)")
             return nil
         }
+
         guard
             let latitude = gps[kCGImagePropertyGPSLatitude] as? Double,
             let latitudeRef = gps[kCGImagePropertyGPSLatitudeRef] as? String,
             let longitude = gps[kCGImagePropertyGPSLongitude] as? Double,
             let longitudeRef = gps[kCGImagePropertyGPSLongitudeRef] as? String
         else {
+            print("Missing GPS Coordinates in metadata from: \(url.lastPathComponent)")
             return nil
         }
         
         let lat = (latitudeRef == "S") ? -latitude : latitude
         let lon = (longitudeRef == "W") ? -longitude : longitude
-        
-        return CLLocationCoordinate2D(latitude: lat, longitude: lon)
+
+        print("📸 Extracted GPS: (\(lat), \(lon)) from: \(url.lastPathComponent)")
+        return Coordinate(latitude: lat, longitude: lon)
     }
 }
